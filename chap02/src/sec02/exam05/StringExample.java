248d449ee42d0929882309124f204413ee05cb30
alleng <bluesky0232@naver.com>
2020-12-01 00:23:42 +0900

    StringExample

---
+++ chap02/src/sec02/exam05/StringExample.java
@@ -0,0 +1,13 @@
+package sec02.exam05;
+	public class StringExample {
+		public static void main(String[] args) {
+			
+			String name = "고선호 高瑄浩";
+			String job = "前 마케터, 現 프로그래머";
+			System.out.println(name);
+			System.out.println(job);
+			
+			String str = "나는 \"자바\"를 공부하고 있습니다.";
+			System.out.println(str);
+		}
+	}
