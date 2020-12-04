b4e249b01a493818da472069b48cb5f4cd0a6de3
alleng <bluesky0232@naver.com>
2020-12-04 19:41:09 +0900

    study

---
+++ chap02/src/sec03/exam03/ByteOperationExample.java
@@ -0,0 +1,6 @@
+package sec03.exam03;
+	public class ByteOperationExample {
+		public static void main(String[]args) {
+			
+		}
+	}
---
+++ chap02/src/test/example/testExample.java
@@ -0,0 +1,17 @@
+package test.example;
+	public class testExample {
+		public static void main(String[]args) {
+			String name = "홍길동";
+			int age = 25;
+			String tel1="010", tel2="123", tel3="4567";
+			// 이름: 김자바
+			// 나이: 25
+			// 전화: 010-123-4567
+			
+			System.out.println("이름: "+ name);
+			System.out.print("나이: "+ age + "\n");
+			// System.out.printf("전화: " + tel1 + "-" + tel2 + "-" + tel3);
+			System.out.printf("전화: %1$s-%2$s-%3$s", tel1, tel2, tel3);
+			
+		}
+	}
